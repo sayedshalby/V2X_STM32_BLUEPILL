1cee5d428ca9337aa14ab87874bab17b8faf2d3f
Sayed Shalby <43934467+sayedshalby@users.noreply.github.com>
2024-04-16 19:44:15 +0200

    Update TL_TIMER_interface.h

--- V2I/TRAFFIC_LIGHT/Inc/TL_TIMER_interface.h
+++ V2I/TRAFFIC_LIGHT/Inc/TL_TIMER_interface.h
@@ -1,10 +1,4 @@
-/***************TL_TIMER Interface***************/
-/**********Author: Mohammed Nagah Amr .**********/
-/********************Ver.: 01********************/
-/****************Date:02/04/2024.****************/
-/************************************************/
-
-#ifndef TL_TIMER_INTERFACE_H_
+ifndef TL_TIMER_INTERFACE_H_
 #define TL_TIMER_INTERFACE_H_
 
 
